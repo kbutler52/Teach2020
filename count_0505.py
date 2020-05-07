ed32d3dd6fac8c556a95a4f6a745e77a146addb7
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 21:03:47 +0000

    count

---
+++ count_0505.py
@@ -0,0 +1,7 @@
+
+count=1 # counter
+while count<=7: # while COUNT is less than or equal to 7 do the below
+    if count%2==0: #if the divient of COUNT equal ZERO print the below
+        print(count) # print this is count Equals Zero
+    count+=1# this is the adder
+
