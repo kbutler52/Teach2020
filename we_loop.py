a450d771169746c3dcaf9fd240465de66f611878
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 21:55:13 +0000

    wholoop

---
+++ we_loop.py
@@ -0,0 +1,9 @@
+count=5 #intialize count
+#while count<=25:#this is not in the code at this time.
+for count in range(1,25):#loops from 1 to 25( but 25 is not included)
+        if count%2==0:#if the value of count is even
+           print(count)#prints the value
+#count+=1#This is not in the program
+count=count+21# This will stop the whil loop after 1 inteation
+
+
