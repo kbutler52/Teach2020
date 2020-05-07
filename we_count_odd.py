7ccb124887faf3fb4c51237cc45759c809e7c086
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 22:05:15 +0000

    odd

---
+++ we_count_odd.py
@@ -0,0 +1,8 @@
+
+count=3# the counter- we are adding t to the counter below
+while count<=7:#while count is less that or equal to 7
+    if count%2==1:#if count divided by 2  is 1 ( odd number)
+        print(count)#if the above is true print count
+    count+=1# this is the counter with out this we will have aloop
+
+
