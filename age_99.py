6a696274eb8d7bdf6fcaaf452b8bb1528eb915f1
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 21:37:40 +0000

    old

---
+++ age_99.py
@@ -0,0 +1,14 @@
+age = 99
+if age >= 35:
+    print ('You are old enough to be a Representative, Senator, or the President.')
+elif age >= 30:
+    print('you are old enough to be a Senator.')
+elif age >= 25:
+    Print('You are old enough to be a Representative.')
+else:
+    print ('you are not old enough to be a Representative, Senator, or the President.')
+print ('Have a NICE day!')
+
+
+
+
