488015be68eb714f02a7fa4cbd0ee2f826ca8565
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 21:12:51 +0000

    countnew

---
+++ count_100.py
@@ -0,0 +1,6 @@
+count=1#this is the numbe we are starting with- part of the counter
+while count<=101:#while count is less than or equal to 101 do this
+    if count%2==0:# if the remainder of count divided by 2 equal 0( means it's a positive number)
+        print(count)#if the above statement is true print count
+    count+=1#my counter
+
