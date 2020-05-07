b72e640e8e4c6a99786139c7d19b4bfda4ffa82a
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 21:28:03 +0000

    dict

---
+++ dic_add.py
@@ -0,0 +1,6 @@
+
+list={'shop': 'fish', 'exercise': 'squats'}#dictionary- has a name and key
+list['shop']='fruit'#this is how to add a item to a dictionary
+things_todo=list['shop']#
+print('I love to eat {}.' .format(things_todo))
+
