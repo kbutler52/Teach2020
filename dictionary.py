751bdfe0c819d3e54d26d952e30637ef32906fa8
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 21:45:24 +0000

    stuff

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+
+list={'shop': 'fish', 'exercise': 'squats'}# dictionary list- dic_name and key
+exercise_todo=list['exercise']#?????
+print('I love to do {} to stay healthy.' .format(exercise_todo))
+
