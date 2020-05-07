67efc08b616e7706eed2c051f64f6474ef540ebe
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 22:18:34 +0000

    loop

---
+++ we_loop2.py
@@ -0,0 +1,6 @@
+
+#count=1#count- start from 1
+for count in range(20,101):#count from 20 until 100
+    if count%2==0:#if the divident iszero and even number
+        print(count)#then print count
+
