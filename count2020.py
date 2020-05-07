a6323f313fc6bff627bcf79cb0831e78606988b0
Kyla Butler <kbutler30@bellsouth.net>
2020-05-07 21:21:23 +0000

    morecount

---
+++ count2020.py
@@ -0,0 +1,8 @@
+
+count=1#starting count
+while count<=7:#while countis less than or equal to 7
+    if count%2==0:#if the remainer of count divided by 2 is zero ( number is even)
+        print(count)#if the above is true print count
+
+    count+=1#This will prevent a loop, - it's a adder
+
