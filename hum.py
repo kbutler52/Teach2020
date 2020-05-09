166d0c29cdca8496cb220b5a99f5c1faef70e1e0
Kyla Butler <kbutler30@bellsouth.net>
2020-05-09 13:40:53 +0000

    if

---
+++ hum.py
@@ -0,0 +1,7 @@
+
+i=0#This is the test expression
+if i  > 15:#if this is true the do the expression just below it
+    print("10 is less that 15")#if the above statement is true- do this
+print("I am Not in if")#if the previous statement was false then we do this- and we did this
+
+
