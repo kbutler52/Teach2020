cbac5d76d83240d47b8e9992d0c3061b33850043
Kyla Butler <kbutler30@bellsouth.net>
2020-05-09 13:47:59 +0000

    fi

---
+++ fi_else.py
@@ -0,0 +1,10 @@
+
+i = 20
+if i< 15:
+    print("i is smaller that 15")
+    print("i'm in if Block")
+else:
+    print("i is greater that 15")
+    print("i'm in else Block")
+
+print("i'm not in if and not in else Block")
