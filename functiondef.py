4ca8cc60fd4034dbfe2ffefec78e7b3b2b53871c
Kyla Butler <kbutler30@bellsouth.net>
2020-05-12 13:59:18 +0000

    funct

---
+++ functiondef.py
@@ -0,0 +1,12 @@
+
+#5-12-2020
+#my very first function that takes arugment and makes something meainingful
+def hi(name):
+    if name =='Ola':
+        print('Hi Ola!')
+    elif name =='Kyla':
+        print('Hi Kyla!')
+    else:
+        print('Hi annoymous!')
+
+hi("Kyla")
