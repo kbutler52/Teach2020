0a8ccde492a3181934dd324f991cf201422dbcc2
Kyla Butler <kbutler30@bellsouth.net>
2020-05-12 15:56:57 +0000

    define

---
+++ defining_fuc.py
@@ -0,0 +1,10 @@
+
+#05-12-2020
+def print_name(name):
+    print(f"Name is {name}")
+
+
+print_name("Kyla")
+
+
+
---
+++ functwo.py
@@ -0,0 +1,13 @@
+
+#05-12-2020
+
+def hi(name):
+    print("Hi"  + name + "!")
+
+def area(side):
+    return side * side
+
+print(area(2))
+
+
+
