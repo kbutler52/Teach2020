cf04c5c4b47f5bb0153922b81c30b710d9e08df4
Kyla Butler <kbutler30@bellsouth.net>
2020-05-18 00:33:51 +0000

    numbs

---
+++ add_num.py
@@ -0,0 +1,8 @@
+
+#05-17-2020
+def love_cat(litter,food,water):
+    print(f"My cat loves {litter},{food} and {water}")
+#below you will see I was able to add a number, but without""s.
+love_cat("sleep",12, "playtime")
+
+
