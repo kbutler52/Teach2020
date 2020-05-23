8b9af0961403e5e20271344c91c669ad3e82ebf7
Kyla Butler <kbutler30@bellsouth.net>
2020-05-23 05:45:16 +0000

    testFor

--- add_num.py
+++ add_num.py
@@ -2,7 +2,6 @@
 #05-17-2020
 def love_cat(litter,food,water):
     print(f"My cat loves {litter},{food} and {water}")
-#below you will see I was able to add a number, but without""s.
-love_cat("sleep",12, "playtime")
 
+love_cat("catfood",200, "playtime")
 
---
+++ testForloop.py
@@ -0,0 +1,11 @@
+
+lemons = 10
+while (lemons >0):
+        print("Mom has  {}  Lemons.". format(lemons))
+        lemons = lemons - 1
+
+
+
+
+
+
