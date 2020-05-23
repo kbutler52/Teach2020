a0c42abc6e125e8b36d033fba7953c55fc37d745
Kyla Butler <kbutler30@bellsouth.net>
2020-05-23 07:17:52 +0000

    for

---
+++ forLemons.py
@@ -0,0 +1,18 @@
+#Lemons = 10
+#contains predefined start,stop,interation
+#runs until stop condition is met
+# Better than while loop
+
+
+
+
+Letters = 15
+for i in range(0, Letters, 5):
+    print(f" Mom has %d pieces of mail" %(i))
+
+
+
+
+
+
+
