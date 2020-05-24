c94c5ee9802a56abd2cc6e69cee100b7e36349f2
Kyla Butler <kbutler30@bellsouth.net>
2020-05-24 06:21:59 +0000

    hatsGloves

---
+++ testFour.py
@@ -0,0 +1,16 @@
+#05-23-2020
+gloves = 5
+hats = 10
+
+print(gloves < hats)#checks if statement
+print(gloves == hats)#checks elif statement
+print(not(gloves < hats))#checks the else statement
+
+if (gloves < hats):
+    print("We have less gloves than hats")
+elif (gloves == hats):
+    print("We have the same amount of gloves and hats")
+else:
+    ("We have more gloves that hats")
+
+
---
+++ testfour.py
@@ -0,0 +1,19 @@
+#05-23-2020
+print(f"We love william {100} %")
+print("We love william %d and he loves me" %(100))
+print("William loves to get {}  on his test." .format(100))
+print(f"We love when William get {100} s on his exams")
+print("We will get all %d on our test this semester." %(100))
+print("We will try harder this year get {} grades." .format(100))
+
+hats = 5
+gloves = 10
+
+print(hats < gloves)#this ckecs our if statement
+print(not(hats < gloves))#this checks our else statement
+
+if(hats < gloves):
+    print("We have less hats than gloves")
+else:
+    print("We have more hats than gloves")
+
