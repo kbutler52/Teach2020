e66fc484351c54e6ee206a2baf5bb6d15cc595bb
Kyla Butler <kbutler30@bellsouth.net>
2020-05-25 00:33:50 +0000

    if

---
+++ oneTest.py
@@ -0,0 +1,15 @@
+#05-24-2020
+#if statement
+
+hats = 20
+gloves = 5
+
+print(hats < gloves)#this checks our if statement
+print(not(hats < gloves))#this checks our else statement
+
+if (hats < gloves):
+    print("We have more hats than gloves")
+else:
+    print("We have %d gloves." %(20))
+
+
