35b5d75da06f7224e96e806c0e2d640f0ef29ab5
kbutler52 <58301523+kbutler52@users.noreply.github.com>
2020-05-24 21:44:33 -0400

    Update whileLLOOP.py

--- whileLLOOP.py
+++ whileLLOOP.py
@@ -1,7 +1,7 @@
 #05-24-2020
 
 kitten = 10
-while kitten < 25:
+while kitten < 25:#this is my condition
     print("We have {} kittens." .format(kitten))
-    kitten = kitten + 1
+    kitten = kitten + 1# this is my counter
 
