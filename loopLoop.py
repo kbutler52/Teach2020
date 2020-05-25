95075b298d924ef729d350105d08db3674774c54
Kyla Butler <kbutler30@bellsouth.net>
2020-05-25 01:29:29 +0000

    loops

---
+++ loopLoop.py
@@ -0,0 +1,6 @@
+#05-24-2020
+cat = 5
+for i in range(0,cat):# this is my start and stop( from 0 to 5)
+    print("We have %d mouses" %(i))#this will print the numbers as they increase
+    cat=cat +1#this is my counter
+
