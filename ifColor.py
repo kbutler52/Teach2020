543c978a3fd2277ca9a08d3ec30893c340391f47
Kyla Butler <kbutler30@bellsouth.net>
2020-05-25 20:43:05 +0000

    redBlue

---
+++ ifColor.py
@@ -0,0 +1,15 @@
+#05-25-2020
+blue = 10
+red = 5
+
+print(blue < red)#this will test check our if statement
+print(not(blue < red))#this will check our elif statement
+print(blue == red)#This will check our else statement
+if (blue < red):#This will check our if statement
+    print(f"Blue is more that red {5}.")
+elif(not(blue < red)):
+    print("Blue is %d more than red." %(5))
+else:
+    print("Blue and Red are equal")
+
+
