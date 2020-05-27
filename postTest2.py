1d5ac5d1bd50edfbdea9281146477eae764eb1ba
Kyla Butler <kbutler30@bellsouth.net>
2020-05-27 19:25:10 +0000

    PostTest2

---
+++ postTest2.py
@@ -0,0 +1,23 @@
+#05-27-2020
+#create a looping statement that
+#prints how many pineapple you have
+#reduces the numberof pineapples by 2
+#repeat until pineapples is less taht 6( flexible)
+
+#this was the answer William came up with, there are many ways to
+#get the same answer
+
+pineapples = 10
+while(pineapples >=6):
+    print(f"I have {pineapples} Pineapples.")
+    pineapples = pineapples - 2
+
+#use input() to store how many cherries you have into a variable called Cherries
+#print how many cherries you have
+#one soultion- William's solution
+cherries = int(input("How many cherries do you have?"))
+print("I have %d cherries" %(cherries))
+
+
+
+
