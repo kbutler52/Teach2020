9253b02aacc43f10af36d2a80868806e15f0b5cb
Kyla Butler <kbutler30@bellsouth.net>
2020-06-03 19:25:15 +0000

    ch5

---
+++ ch5.py
@@ -0,0 +1,25 @@
+
+#06-03-2020
+#practice using input
+
+#Write a program that uses the input() function to prompt the user to entr an
+#integer
+#into a variable called num1.
+#next,prompt the user to enter another integer and store the input
+#into another variable called num2.
+#use the print() function to display the message.
+
+num1 = input("Enter an integer:")
+num2 = input("Enter another integer:")
+print('You have entered %s and %s.' %(num1,num2))
+
+num3= input('Please enter a number from 1 to 5: ')
+num4 = input('Please enter another number from 6 to 10: ')
+print('You have enter %s and %s.' %(num3, num4))
+
+num5 = input('Enter integer: ')
+num6 = input('Enter a second integer: ')
+print('You have entered %s and %s.' %(num5,num6))
+
+
+
