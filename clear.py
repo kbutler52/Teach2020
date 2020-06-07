67f6b6d53f623094859d5e2adca148cca6d5a9a6
Kyla Butler <kbutler30@bellsouth.net>
2020-06-07 01:54:59 +0000

    training

---
+++ clear.py
@@ -0,0 +1,17 @@
+#06-06-2020
+#this is me at work running thur stuff I learned on my own
+#input
+name = input("Please enter your name: ")
+favNum =input("%s,Please enter your favorite number: " %(name))
+print("%s \'s favorite number is %s." %(name,favNum))
+
+
+name = input('Enter your cat\'s name: ')
+favNum = input('Enter %s \'s favorite number: ' %(name))
+print('%s \'s favorite number is %s.' %(name,favNum))
+
+
+name = input('Plese enter your first name: ')
+favColor= input('%s enter your favorite color:' %(name))
+print('%s\'s favorite color is %s.' %(name,favColor)
+
