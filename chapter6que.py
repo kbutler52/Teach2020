4904d86fbaf3b457a4b0b853cb793601672088f9
Kyla Butler <kbutler30@bellsouth.net>
2020-06-10 18:36:27 +0000

    chapter6

---
+++ chapter6que.py
@@ -0,0 +1,16 @@
+#06-10-2020
+#Chapter6 questions
+#Quetion 1) State which of the following statements are true:
+#b,c,d,e,g,i,j,k
+
+#Question 2)
+#Determine the output of the following program without running the code:
+
+num = 5
+if (num == 1):
+    print('num is 1')
+elif (num == 2):
+    print('num is 2')
+else:
+    print('num is neither 1 nor 2')
+
